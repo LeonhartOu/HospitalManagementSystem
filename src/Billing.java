c19025ba113bbd8a30d68f6791b0bd7bd7b163c7
Leon <90753454+LeonhartOu@users.noreply.github.com>
2022-12-27 19:49:52 +0700

    Update Billing.java

--- src/Billing.java
+++ src/Billing.java
@@ -1,3 +1,52 @@
 public class Billing {
+	private String billID;
+	private String billingDate;
+	
+	private Patient patient;
+	private Doctor doctor;
+	private Pharmacist pharmacist;
+	private Prescription prescription;
+	
+	private int totalPrice;
+
+	public Billing(String billID, String billingDate, Patient patient, Doctor doctor, Pharmacist pharmacist, Prescription prescription, int totalPrice){
+		this.billID = billID;
+		this.billingDate = billingDate;
+		
+		this.patient = patient;
+		this.doctor = doctor;
+		this.pharmacist = pharmacist;
+		this.prescription = prescription;
+
+		this.totalPrice = totalPrice;
+	}
+	
+	public String getbillID() {
+		return billID;
+	}
+	
+	public String billingDate() {
+		return billingDate;
+	}
+	
+	public Patient patient() {
+		return patient;
+	}
+	
+	public Doctor doctor() {
+		return doctor;
+	}
+	
+	public Pharmacist pharmacist() {
+		return pharmacist;
+	}
+
+	public Prescription prescription() {
+		return prescription;
+	}
+	
+	public int totalPrice() {
+		return totalPrice;
+	}
     
 }
